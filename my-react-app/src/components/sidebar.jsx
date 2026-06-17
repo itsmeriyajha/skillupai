e78797e588f67d5b3ce74cb9ef2588c208cb20b8
itsmeriyajha <riyajha082004@gmail.com>
2026-06-18 01:48:01 +0530

    Changes

--- my-react-app/src/components/sidebar.jsx
+++ my-react-app/src/components/sidebar.jsx
@@ -1,23 +1,20 @@
 
 import styles from "./sidebar.module.css"
 import { memo } from 'react';
-import { Link, useNavigate } from "react-router-dom";
+import {Link,useNavigate}from "react-router-dom";
 
 const sidebar = () => {
-  const navigate = useNavigate();
+  const navigate=useNavigate();
 
-  const dashboardhandle = () => {
+  const dashboardhandle=()=>{
     navigate('/dashboard')
   };
-  const myprofile = () => {
+  const myprofile=()=>{
     navigate('/profile')
   };
-
   return (
-    <div className={styles.sidebar}>
-      {/* Brand Logo inside the sidebar */}
-      <h2 className={styles.logo}>SkillUp AI</h2>
 
+    <div>
       <div className={styles.profile}>
         <div className={styles.avatar}></div>
         <h4>Riya</h4>
